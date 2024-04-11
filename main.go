5815a361ff23b44655f2c76b89807066313c0af3
mpomirski <michal.pomirski01@gmail.com>
2024-04-11 21:09:24 +0200

    Finished

--- main.go
+++ main.go
@@ -9,69 +9,67 @@ import (
 	"strings"
 )
 
-func factorial(n int) big.Int {
-    var value big.Int = *big.NewInt(1);
-    for i := n; i > 0; i-- {
-        value = *value.Mul(&value, big.NewInt(int64(i)));
-    }
-    return value;
+func factorial(n int) *big.Int {
+	value := big.NewInt(1)
+	for i := n; i > 0; i-- {
+		value.Mul(value, big.NewInt(int64(i)))
+	}
+	return value
 }
 
 func fibRecursive(n int, counter map[int]int) (int, map[int]int) {
-	if n == 0 || n == 1{
-		counter[n] += 1;
-		return 1, counter;
+	counter[n]++
+	if n == 0 || n == 1 {
+		return 1, counter
 	}
-	counter[n] += 1;
-	result1, counter1 := fibRecursive(n-1, counter);
-	result2, counter2 := fibRecursive(n-2, counter1);
-	return result1 + result2, counter2;
+	result1, counter1 := fibRecursive(n-1, counter)
+	result2, counter2 := fibRecursive(n-2, counter1)
+	return result1 + result2, counter2
 }
 
 func strToAscii(value string) []byte {
-    return []byte(value);
+	return []byte(value)
 }
 
 func isAliasValueInNFactorial(alias string, n int) bool {
-    ascii := strToAscii(alias);
-	result := true;
-	factorialValue := factorial(n);
-    for _, char := range(ascii){
-        if (!strings.Contains(factorialValue.String(), strconv.FormatInt(int64(char), 10))) {
-			result = false;
+	ascii := strToAscii(alias)
+	factorialValue := factorial(n)
+	for _, char := range ascii {
+		if !strings.Contains(factorialValue.String(), strconv.FormatInt(int64(char), 10)) {
+			return false
 		}
-    }
-	return result;
+	}
+	return true
 }
 
 func findStrongNumber(fullName string) (int, error) {
-	fullName = strings.ToLower(fullName);
-	firstName := strings.Split(fullName, " ")[0];
-	lastName := strings.Split(fullName, " ")[1];
-	alias := firstName[:3] + lastName[:3];
-	for n := 1; !isAliasValueInNFactorial(alias, n-1); n++{
+	fullName = strings.ToLower(fullName)
+	names := strings.Split(fullName, " ")
+	if len(names) < 2 {
+		return 0, errors.New("invalid full name")
+	}
+	firstName := names[0]
+	lastName := names[1]
+	alias := firstName[:3] + lastName[:3]
+	for n := 1; !isAliasValueInNFactorial(alias, n-1); n++ {
 		if isAliasValueInNFactorial(alias, n) {
-			return n, nil;
+			return n, nil
 		}
 	}
-	return 0, errors.New("Something went wrong");
+	return 0, errors.New("strong number not found")
 }
+
 func main() {
-	fullName := "Michał Pomirski";
-	strongNumber, err := findStrongNumber(fullName);
-	if err != nil{
-		log.Fatal(err);
+	fullName := "Michał Pomirski"
+	strongNumber, err := findStrongNumber(fullName)
+	if err != nil {
+		log.Fatal(err)
 	}
-	fmt.Printf("Twoja Silna liczba to: %d\n", strongNumber);
-	// Moja Silna liczba: 247
+	fmt.Printf("Twoja Silna liczba to: %d\n", strongNumber)
 
-	a, b := fibRecursive(30, make(map[int]int));
-	fmt.Println("Kolejne ilości wywołań funkcji fib dla fib(30):");
-	fmt.Printf("fib(%d): Wartość: %d\nIlości: %d\n", 30, a, b);
-	fmt.Println("Twoja słaba liczba to argument przy wartości najbliższej do liczby wywołań danego argumentu funkcji fib(30).");
+	a, b := fibRecursive(30, make(map[int]int))
+	fmt.Println("Kolejne ilości wywołań funkcji fib dla fib(30):")
+	fmt.Printf("fib(%d): Wartość: %d\nIlości: %d\n", 30, a, b)
+	fmt.Println("Twoja słaba liczba to argument przy wartości najbliższej do liczby wywołań danego argumentu funkcji fib(30).")
 	fmt.Println("Np. dla Silnej liczby 247, najbliższa wartość to 233, więc słaba liczba to 18 [233 razy]")
-	//Najbliższa wartość do 247, to ilość wywołań fib(18): 233
-	// Moja słaba liczba: 30 [233 razy]
 }
-
-
